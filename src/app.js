34d082bc8faa3f0b2dd5ba83b1a35fdf03fe6178
prataprohit526 <prataprohit526@gmail.com>
2023-04-15 12:12:12 +0530

    add all

--- src/app.js
+++ src/app.js
@@ -8,17 +8,43 @@ const server = http.createServer((req, res) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks);
+    });
+
+    req.on("end", () => {
+      const obj = JSON.parse(chunks.join(""));
       const value1 = obj.num1;
       const value2 = obj.num2;
 
-      // Write code here to calculate power of a number
+      // Validate input
+      if (value1 <= 0 || !Number.isInteger(value1)) {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ error: "num1 must be a positive integer" }));
+        return;
+      }
+
+      if (value2 < 0 || !Number.isInteger(value2)) {
+        res.statusCode = 400;
+        res.setHeader("Content-Type", "application/json");
+        res.end(
+          JSON.stringify({ error: "num2 must be a non-negative integer" })
+        );
+        return;
+      }
+
+      // Calculate result
       const result = Math.pow(value1, value2);
 
-      // Send the result back as the response
-      res.writeHead(404, { "Content-Type": "application/json" });
+      // Send response
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify({ result }));
     });
+  } else {
+    // Handle invalid HTTP method
+    res.statusCode = 405;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "Invalid HTTP method" }));
   }
 });
 
