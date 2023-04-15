c14b3c4bf193211b7ca184dd26a257556207bff0
prataprohit526 <prataprohit526@gmail.com>
2023-04-15 11:57:34 +0530

    add all

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const chunks = [];
 
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
+      const obj = JSON.parse(chunks);
       const value1 = obj.num1;
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      
+      const result = Math.pow(value1, value2);
+
+      // Send the result back as the response
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ result }));
     });
-    }
+  }
 });
 
 module.exports = server;
-      
